3d74001b8b01758009507143ecfeb476e146a33c
Addi Abel <addi.abel@digital.landregistry.gov.uk>
2016-11-23 11:57:04 +0000

    mis-merged a merge conflict

--- manifests/c_params.pp
+++ manifests/c_params.pp
@@ -11,16 +11,14 @@
 #
 class nagiosclient::c_params {
 
-  $c_package_name                        = 'nagios-client'
-  $c_user                                = 'nagios'
-  $c_group                               = 'nagios'
-  $c_userid                              = '240'
-  $c_grpuid                              = '240'
-  $c_home_dir                            = '/var/spool/nagios'
-  $c_config_dir                          = '/etc/nagios'
-  $c_plugin_dir                          = hiera('nagios_plugin_dir')
-  $c_nagios_server                       = hiera('nagios_server_name')
-  $c_nagios_server_ip                    = hiera('nagios_server_ip')
+  $c_package_name     = 'nagios-client'
+  $c_user             = 'nagios'
+  $c_group            = 'nagios'
+  $c_userid           = '998'
+  $c_grpuid           = '997'
+  $c_home_dir         = '/var/spool/nagios'
+  $c_config_dir       = '/etc/nagios'
+  $c_plugin_dir       = '/var/lib64/nagios/plugins/'
   }
 
 
